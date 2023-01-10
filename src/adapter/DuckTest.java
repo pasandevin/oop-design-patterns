cd39869ec6c0a4b5e0cc71b49cd58ea55466f0b9
pasandevin <jayawardene.pasandevin@gmail.com>
2023-01-10 19:44:59 +0530

    implement adapter for duck problem

---
+++ src/adapter/DuckTest.java
@@ -0,0 +1,21 @@
+package adapter;
+
+public class DuckTest {
+    public static void main(String[] args) {
+        MallardDuck duck = new MallardDuck();
+
+        WildTurkey turkey = new WildTurkey();
+        Duck turkeyAdapter = new TurkeyAdapter(turkey);
+
+        turkey.gobble();
+        turkey.fly();
+
+        testDuck(duck);
+        testDuck(turkeyAdapter);
+    }
+
+    static void testDuck(Duck duck) {
+        duck.quack();
+        duck.fly();
+    }
+}
