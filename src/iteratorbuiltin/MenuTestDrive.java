df9039fe215e69ade7f0c263992e6054283cc57d
pasandevin <jayawardene.pasandevin@gmail.com>
2023-03-06 18:00:46 +0530

    add new menu

--- src/iteratorbuiltin/MenuTestDrive.java
+++ src/iteratorbuiltin/MenuTestDrive.java
@@ -1,5 +1,6 @@
 package iteratorbuiltin;
 
+import iteratorbuiltin.aggregate.CafeMenu;
 import iteratorbuiltin.aggregate.DinerMenu;
 import iteratorbuiltin.aggregate.Menu;
 import iteratorbuiltin.aggregate.PancakeHouseMenu;
@@ -8,8 +9,9 @@ public class MenuTestDrive {
     public static void main(String[] args) {
         Menu pancakeHouseMenu = (Menu) new PancakeHouseMenu();
         Menu dinerMenu = (Menu) new DinerMenu();
+        Menu cafeMenu = (Menu) new CafeMenu();
 
-        Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu);
+        Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu, cafeMenu);
 
         waitress.printMenu();
     }
